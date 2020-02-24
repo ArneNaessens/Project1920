a44f7e7c638482988a2284f95adfe813fd80d1fe
ArneNaessens <arnenaessens@hotmail.com>
2020-02-24 12:18:56 +0000

    First install command

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+function command_exists() {
+	command -v "$@" >/dev/null 2>&1
+}
+
+mainmenu_selection=$(whiptail --title "Main Menu" --menu --notags \
+	"" 20 78 12 -- \
+	"docker" "Install Docker" \
+	"commands" "Docker commands" \
+	3>&1 1>&2 2>&3)
+case $mainmenu_selection in
+"docker")
+	if command_exists docker; then
+		echo "docker already installed"
+	else
+		echo "Install Docker"
+		curl -fsSL https://get.docker.com | sh
+		sudo usermod -aG docker $USER
+	fi
+
+	if command_exists docker-compose; then
+		echo "docker-compose already installed"
+	else
+		echo "Install docker-compose"
+		sudo apt install -y docker-compose
+	fi
+
+	if (whiptail --title "Restart Required" --yesno "It is recommended that you restart you device now. Select yes to do so now" 20 78); then
+		sudo reboot
+	fi
+;;
+*) ;;
+esac
