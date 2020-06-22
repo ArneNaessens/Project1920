1118e7303e8e1d5c3d24554f34a82fd348902704
Gregory Desktop <gregory.martens@student.odisee.be>
2020-06-22 18:34:31 +0200

    changes

--- setup_client.sh
+++ setup_client.sh
@@ -35,16 +35,13 @@ case $mainmenu_selection in
     cd ~ || return
 	sudo apt-get install cmake libusb-1.0-0-dev build-essential
     git clone git://git.osmocom.org/rtl-sdr.git
-    cd rtl-sdr/ && mkdir build && cd build/ || return
-    cmake ../ -DINSTALL_UDEV_RULES=ON
+    cd rtl-sdr/ 
+	mkdir build 
+	cd build/ 
+    cmake ../
     sudo make
     sudo make install
     sudo ldconfig
-    cd ~ || return
-    sudo cp ./rtl-sdr/rtl-sdr.rules /etc/udev/rules.d/
-    "blacklist dvb_usb_rtl28xxu
-    blacklist rtl2832
-    blacklist rtl2830" >> /etc/modprobe.d/no-rtl.conf
 
 	if (whiptail --title "Restart Required" --yesno "It is recommended that you restart you device now. Select yes to do so now" 20 78); then
 		sudo reboot
@@ -57,11 +54,13 @@ case $mainmenu_selection in
 	sudo apt-get install libtool libusb-1.0.0-dev librtlsdr-dev
 	rtl-sdr doxygen
 	git clone https://github.com/merbanan/rtl_433.git
-	cd rtl_433/ && mkdir build &&
-	cd build && cmake ../ &&
+	cd rtl_433/
+	mkdir build
+	cd build 
+	cmake ../
 	make
 	sudo make install
-	sudo apt-get install -y supervisor
+	sudo yum install -y supervisor
 ;;
 
 "setting")
@@ -69,17 +68,7 @@ case $mainmenu_selection in
 	username=$(whiptail --inputbox "Username for MQTT server" 8 78 admin --title "Username" 3>&1 1>&2 2>&3)
 	password=$(whiptail --passwordbox "please enter your secret password for MQTT server" 8 78 --title "password" 3>&1 1>&2 2>&3)
 
-	sudo echo '
-	[program:rtl_433]
-	command=/home/pi/rtl_433/build/src/rtl_433 -R 123 -F “mqtt://${server}:1883,,user=”${username}”,pass=”${password}”,events=BEER”user=pi
-	autostart=yes
-	autorestart=yes
-	startretries=100
-	stderr_logfile=/var/log/rtl_433/rtl_433.err.log
-	stdout_logfile=/var/log/rtl_433/rtl_433.log
-	' sudo tee -a /etc/supervisor/conf.d/rtl_433.conf
 	sudo mkdir /var/log/rtl_433
-	sudo service supervisor start
 ;;
 
 "start")
